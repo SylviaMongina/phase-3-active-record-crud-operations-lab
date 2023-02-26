8d44cb0379fdc8af441eb14e781629e7e46cbbb3
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-02-26 20:34:47 +0300

    completed assignment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,5 @@
 class Movie < ActiveRecord::Base
-    def self.find_all_movies_by_year(year)
-        Movie.where(year: year)
-    end
+
     def self.create_with_title(title)
         Movie.create(title: title)
     end
@@ -9,42 +7,41 @@ class Movie < ActiveRecord::Base
     def self.first_movie
         Movie.first
     end
-    
+
     def self.last_movie
         Movie.last
     end
-    
+
     def self.movie_count
-        Movie.count
+        self.count
     end
 
     def self.find_movie_with_id(id)
-        Movie.find(id)
+        self.find_by_id(id)
     end
-    
+
     def self.find_movie_with_attributes(attributes)
-        Movie.find_by(attributes)
+        self.find_by(attributes)
     end
-    
+
     def self.find_movies_after_2002
-        Movie.where("release_date > 2002")
+        Movie.where("release_date > ?", 2002)
     end
-    
-    def update_with_attributes(attributes)
-        self.update(attributes)
+
+    def update_with_attributes(attribute)
+        self.update(attribute)
     end
-    
+
     def self.update_all_titles(title)
         Movie.update(title: title)
     end
-    
+
     def self.delete_by_id(id)
-        movie = Movie.find(id)
-        movie.destroy
+        Movie.delete(id)
     end
-    
+
     def self.delete_all_movies
-        Movie.destroy_all
+        Movie.delete_all
     end
-    
+
 end
